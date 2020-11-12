ace74b18b383aed0fb374545a51510ccd0ee3d95
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-11-12 10:30:44 +0100

    Minor cleanup of dataset doc

--- R/data.R
+++ R/data.R
@@ -1,6 +1,8 @@
 #' Allele frequencies
-#' A dataset `freqsBlind` containing allele frequencies
 #'
-#' @format A list with 198 elements, markers.  The first 27 are for STRs, the remaining for SNps
-#' 
+#' A dataset containing allele frequencies for 198 markers.
+#'
+#' @format A list of length 198. Each element is a vector of allele frequencies
+#'   for a single marker. The first 27 markers are STRs, the remaining are SNPs.
+#'   
 "freqsBlind"
