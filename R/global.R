062c98dd6d7cc7e79ccb2439416902b2b4125d5d
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-11-14 16:17:58 +0100

    Improve code in examples

--- R/global.R
+++ R/global.R
@@ -21,61 +21,66 @@
 #' @import pedtools
 #' @import forrel
 #' @importFrom pedprobr likelihood
+#' 
 #' @examples
-#' \dontrun{
-#' # The data is specified and plotted
-#' n = 7 #Number of victims
-#' ids.from = paste("V", 1:n, sep = "")
-#' sex = c(rep(1, n-1), 2)
-#' df = data.frame(id = ids.from, fid = 0, mid = 0, sex = sex,
-#'                 a1 = c(1,2,1,1,2,2,2),
-#'                 a2 = c(1,2,1,1,2,2,2))
-#' locus_annotations = list(alleles = 1:3, afreq = c(1, 1, 1)/3)
-#' from = as.ped(df, locusAttributes = locus_annotations)
-#' to = nuclearPed(3, father = "R1", mother = "R2", children= c("MP1","MP2","MP3"))
-#' m = marker(to, "R1" = 1, "R2" = 1,   alleles = 1:3, afreq = c(1, 1, 1)/3, name = "a1")
-#' to = addMarkers(to, m)
-#' MPs = c("MP1", "MP2","MP3")
-#' plotPedList(list(from, to), marker = 1, titles = c("PM data", "AM data"))
+#' \donttest{
+#' library(forrel)
 #' 
-#' # All sex consistent assignments are considered
-#' res1 = global(from, to, MPs, moves = NULL, limit = -1, verbose = F)
+#' ### Example 1 ###
 #' 
-#' # Only three best marginal moves are considered
-#' moves = generateMoves(from, to, MPs) # generate all sex consistent assignments
-#' moves2 = marginal(from, to,  ids.to, moves, limit = -1, sorter = T,  nkeep = 3)
-#' res2 = global(from, to, MPs, moves2[[1]], limit = -1, verbose = F)
+#' # Attributes of a single marker
+#' locAttr = list(name = "m", alleles = 1:3, afreq = c(1, 1, 1)/3)
 #' 
-#' # Only V1, V3, V4
-#' moves2 = moves[c(2,4,5)]
+#' # PM data (victims: 6 males, 1 female)
+#' vics = paste0("V", 1:7)
+#' sex = c(1,1,1,1,1,1,2)
+#' df = data.frame(id = vics, fid = 0, mid = 0, sex = sex,
+#'                 m = c("1/1", "2/2", "1/1", "1/1", "2/2", "2/2", "2/2"))
+#' from = as.ped(df, locusAttributes = locAttr)
+#' 
+#' # AM data (families)
+#' MPs = c("MP1", "MP2", "MP3")
+#' to = nuclearPed(3, father = "R1", mother = "R2", children = MPs)
+#' m = marker(to, "R1" = "1/1", "R2" = "1/1", name = "m")
+#' to = setMarkers(to, m, locusAttributes = locAttr)
+#' 
+#' # Plot
+#' plotPedList(list(from, to), marker = 1, col = list(red = MPs),
+#'             titles = c("PM data", "AM data"))
+#'  
+#' # Analysis considering all sex-consistent assignments
+#' res1 = global(from, to, MPs, moves = NULL, limit = -1, verbose = FALSE)
+#' 
+#' # Quicker alternative: Consider only the three best moves for each victim
+#' moves = generateMoves(from, to, MPs) # generate all sex-consistent assignments
+#' moves2 = marginal(from, to,  MPs, moves, limit = -1, sorter = TRUE,  nkeep = 3)
+#' res2 = global(from, to, MPs, moves2[[1]], limit = -1, verbose = FALSE)
+#' 
+#' # Further reduction: Only consider victims V1, V3 and V4
+#' moves2 = moves[c("V1", "V3", "V4")]
 #' res = global(from, to, MPs, moves2, limit = -1)
-#' moves = moves[1]
-#' res = global(from, to, MPs, moves, limit = 0)
-#'
-#' # Example 2
+#' 
+#' ### Example 2 ###
 #' # Checked against; http://familias.name/BookKEP/globalExample2.fam
-#' V1 = singleton("V1")
-#' m = marker(name = "m1", V1, alleles = 1:10, "V1" = c(1,1))
-#' V1 = setMarkers(V1, m)
-#' V2 = singleton("V2")
-#' m = marker(name = "m1", V2, alleles = 1:10, "V2" = c(1,2))
-#' V2 = setMarkers(V2, m)
-#' V3 = singleton("V3",2)
-#' m = marker(name = "m1", V3, alleles = 1:10, "V3" = c(3,4))
-#' V3 = setMarkers(V3, m)
-#' V4 = singleton("V4")
-#' m = marker(name = "m1", V4, alleles = 1:10, "V4" = c(3,4))
-#' V4 = setMarkers(V4, m)
-#' from = list(V1 = V1, V2 = V2, V3 = V3, V4 = V4)
-#' AM1 = nuclearPed(1, father = "MP1", child = "MP2", mother ="R1")
-#' m = marker(name = "m1",AM1, alleles = 1:10, "R1" = c(2, 2))
-#' AM1 = setMarkers(AM1, m)
-#' AM2 = halfSibPed(sex1 = 1, sex2 = 2)
-#' AM2 = relabel(AM2, c("R2", "MO2", "FA2", "MP3", "MP4"), 1:5)
-#' m = marker(name = "m1", AM2, alleles = 1:10, "R2" = c(3,3))
-#' AM2 = setMarkers(AM2, m)
-#' to = list(AM1 = AM1, AM2 = AM2)
-#' plotPedList(list(from, to), marker = "m1", shaded = typedMembers)
+#' 
+#' # Single locus with 10 alleles
+#' loc = list(alleles = 1:10)
+#' 
+#' # Victims: 3 males, 1 female
+#' vics = paste0("V", 1:4)
+#' sex = c(1, 1, 2, 1)
+#' df = data.frame(famid = vics, id = vics, fid = 0, mid = 0, sex = sex,                 
+#'                 m1 = c("1/1", "1/2", "3/4", "3/4"))
+#' from = as.ped(df, locusAttributes = loc)
+#'                 
+#' # Reference families: 4 missing persons; 2 genotyped relatives
+#' fam1 = nuclearPed(1, father = "MP1", child = "MP2", mother ="R1")
+#' fam2 = halfSibPed(sex1 = 1, sex2 = 2)
+#' fam2 = relabel(fam2, c("MO2", "R2", "MO3", "MP3", "MP4"))
+#' data = data.frame(m1 = c(R1 = "2/2", R2 = "3/3"))
+#' to = setMarkers(list(fam1, fam2), alleleMatrix = data, locusAttributes = loc)
+#' 
+#' # Generate sex-consistent moves
 #' ids.to = c("MP1", "MP2", "MP3", "MP4") # user specified
 #' males = ids.to[1:3]
 #' females = ids.to[4]
@@ -83,23 +88,22 @@
 #'              V2 = c("V2", males),
 #'              V3 = c("V3", females),
 #'              V4 = c("V4", males))
-#' res = global(from, to, ids.to, moves = moves, limit = 0, verbose = T)
+#'              
+#' # Rank according to likelihood
+#' res1 = global(from, to, ids.to, moves = moves, limit = 0, verbose = TRUE)
 #' resmarg = marginal(from, to, ids.to, moves = moves, limit = 0,
-#'              verbose = T, sorter = TRUE, nkeep = 2)
-#' res = global(from, to, ids.to, moves = resmarg[[1]], limit = 0, verbose = T)
+#'              verbose = TRUE, sorter = TRUE, nkeep = 2)
+#' res2 = global(from, to, ids.to, moves = resmarg[[1]], limit = 0, verbose = TRUE)
 #'
-#' #From fam file
+#' # From fam file
 #' x = readFam("http://familias.name/BookKEP/globalExample2.fam")
-#' from = x[[1]][1:4]
-#' to = x[[1]][5:6]
-#' plotPedList(list(from,to))
-#' ids.to = c("MP1", "MP2", "MP3", "MP4") # user specified
-#' males = ids.to[1:3]
-#' females = ids.to[4]
-#' moves = list(V1 = c("V1", males), V2 = c("V2", males),
-#'              V3 = c("V3", females), V4 = c("V4", males))
-#' res = global(from, to, ids.to, moves = moves, limit = 0, verbose = T)
-#'
+#' from = x$families[1:4]
+#' to = x$families[5:6]
+#' plotPedList(list(from, to))
+#' res3 = global(from, to, ids.to, moves = moves, limit = 0, verbose = TRUE)
+#' 
+#' stopifnot(identical(res1, res3))
+#' 
 #' }
 #' @export global checkDVI
 
@@ -110,7 +114,9 @@ global = function(from, to,  ids.to, moves = NULL, limit = 0, verbose = F){
     moves = generateMoves(from = from, to = to,  ids.to = ids.to)
   else # remove elements with missing, i.e.if e.g. V = NA
     moves = moves[unlist(lapply(moves, function(x) !any(is.na(x))))]
-  res = checkDVI(from = from, to = to,  ids.to = ids.to , moves = moves)
+  
+  # Check consistency
+  checkDVI(from = from, to = to,  ids.to = ids.to , moves = moves)
   
   ids.from = unlist(labels(from))  # as.character(lapply(from, function(x) x$ID))
   marks = seq_len(nMarkers(from))  # 1:nMarkers(from)
@@ -130,8 +136,9 @@ global = function(from, to,  ids.to, moves = NULL, limit = 0, verbose = F){
     stop("No possible solutions specified, possibly identical moves")
   loglik = LR = rep(NA, nm)
 
+  # Loop through moves
   for (i in 1:nm){
-    if(verbose) cat("iteration ", i, "of ", nm, "\n")
+    if(verbose) cat("iteration", i, "of", nm, "\n")
     thisMove = setdiff(moves2[[i]], tomove) #only for identity move, improve code?
     if(length(thisMove) == 0)
         loglik[i] = loglik0
@@ -167,8 +174,8 @@ global = function(from, to,  ids.to, moves = NULL, limit = 0, verbose = F){
   tab = data.frame(moves2,loglik = loglik, LR = LR, posterior = posterior )
   
   # Sort in decreasing order
-  ord = order(loglik, decreasing = T)
-  tab = tab[ord,]
+  tab = tab[order(loglik, decreasing = T), , drop = FALSE]
+  rownames(tab) = NULL
   tab
 }
 
@@ -198,7 +205,7 @@ checkDVI = function(from, to,  ids.to, moves){
     if(length(candidates) == 0 & idsMoves[[i]] != idsMoves[i])
       stop("Wrong identity move in element ", i, " of moves")
   }
-  NULL
+  
 }
 
   
