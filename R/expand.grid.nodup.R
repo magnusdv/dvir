744e601e4678c2b6800efe0dcb613e427dcebccd
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-11-26 23:52:43 +0100

    `expand.grid.nodup()` returns data frame

--- R/expand.grid.nodup.R
+++ R/expand.grid.nodup.R
@@ -1,48 +1,37 @@
-#' Generate all assignment combinations
+#' Combinations without duplications
 #'
-#' Given a list of possible assignments from victims to missing
-#' persons, all unique combinations are generated.
+#' This is a simple extension of [expand.grid()] which removes all combinations with repeated elements.
 #' 
-#' @param lst List of possible moves.
+#' @param lst A list of vectors.
 #' 
 #' @author Magnus Dehli Vigeland
 #' 
-#' @return List of named vectors.
-#' @export
+#' @return A data frame.
+#' @seealso [expand.grid()]
 #' 
 #' @examples
 #' 
-#' # Example 1
-#' moves = list(V1 = c("V1", "MP1"), V2 = c("V2", "MP2"))
-#' expand.grid.nodup(moves)
-#' expand.grid.nodup(moves[1])
-#'
-#' # Example 2
-#' expand.grid.nodup(list(c(1,2,4), c(1,3), 2))
+#' lst = list(1:2, 1:2)
 #' 
-#' # Example 3
-#' expand.grid.nodup(list(c(12, 1), 2))
+#' # Compare
+#' expand.grid.nodup(lst)
+#' expand.grid(lst)
 #' 
-#' \dontrun{
-#' lst = rep(list(1:12), 5)
-#' gr = expand.grid.nodup(lst)   # takes a few seconds
-#' length(gr)   # 95040
-#' }
 #' 
+#' @export
 expand.grid.nodup = function(lst) {
   stopifnot(is.list(lst))
-
+  
   # Grid
   args = c(lst, list(KEEP.OUT.ATTRS = F, stringsAsFactors = F))
   full.grid = do.call(expand.grid, args)
-
-  # Which rows have no duplications?
-  nodups = apply(full.grid, 1, anyDuplicated) == 0
   
-  # Good combinations:
-  good.grid = full.grid[nodups, , drop = F]
-
-  # Convert from data frame to list
-  lapply(seq_len(nrow(good.grid)), 
-         function(i) unlist(good.grid[i, , drop = F], use.names = TRUE))
+  # Remove rows with repeated elements
+  nodups = apply(full.grid, 1, anyDuplicated, incomparables = "*") == 0
+  res = full.grid[nodups, , drop = F]
+  
+  # Fix row names
+  rownames(res) = NULL
+  
+  res
 }
