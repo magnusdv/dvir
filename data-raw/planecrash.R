318a4127b5c7266b746e4ccba9d6e0dd99939c65
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-11-27 18:54:21 +0100

    Add dataset `planecrash`

---
+++ data-raw/planecrash.R
@@ -0,0 +1,36 @@
+# Dataset based on Exercise 3.3 EKM (2015)
+
+library(pedtools)
+
+# Load fam file
+x = forrel::readFam("data-raw/Solution3_3.fam")
+
+### PM data
+pm = x$`Unidentified persons`
+
+# Rename singletons: victims
+victims = paste0("V", 1:8)
+names(pm) = victims
+pm = relabel(pm, old =  paste0("PM", 1:8), new = victims)
+
+# Change sex of first 6 victims, so that all 8 are female
+pm[1:6] = lapply(pm[1:6], function(v) swapSex(v, labels(v)))
+
+### AM data
+MPs = paste0("MP", 1:5)
+refs = paste0("R", 1:5)
+
+am = lapply(1:5, function(i) {
+  fam = x[[paste("Family", i)]][["Reference pedigree"]]
+  
+  # Relabel typed member to R1, R2, ... (each family has just one)
+  fam = relabel(fam, old = typedMembers(fam), new = refs[i])
+  
+  # Relabel missing person
+  relabel(fam, old = "Missing person", new = MPs[i])
+})
+
+# Collect and save
+planecrash = list(pm = pm, am = am, MPs = MPs)
+
+usethis::use_data(planecrash, overwrite = TRUE)
