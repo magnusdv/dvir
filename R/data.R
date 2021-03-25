a644cc7e624ea70260965f27f7d28f10210283a9
Thore <thore.egeland@gmail.com>
2021-03-25 08:17:29 +0100

    Proof read

--- R/data.R
+++ R/data.R
@@ -1,14 +1,3 @@
-#' Allele frequencies
-#'
-#' A dataset containing allele frequencies for 198 markers.
-#'
-#' @format A list of length 198. Each element is a vector of allele frequencies
-#'   for a single marker. The first 27 markers are STRs, the remaining are SNPs.
-#'   
-"freqsBlind"
-
-
-
 #' Data. Stepwise identification may fail
 #'
 #' A proof-of-concept dataset showing that showing that stepwise victim
@@ -43,10 +32,10 @@
 #' pm = example2$pm
 #' am = example2$am
 #' missing = example2$missing
-#' jointDVI(pm, am, missing,)
+#' jointDVI(pm, am, missing)
 "example2"
 
-#' Data. planecrash
+#' Data, simulated planecrash
 #'
 #' DVI dataset based on simulated data in Exercise 3.3 in Egeland et al.
 #' "Relationship Inference with Familias and R" (2015).
@@ -62,7 +51,7 @@
 #'   
 "planecrash"
 
-#' DVI example with large reference pedigree
+#' Data. DVI example with large reference pedigree
 #'
 #' DVI dataset based loosely on the ICMP workshop material
 #' http://www.few.vu.nl/~ksn560/Block-III-PartI-KS-ISFG2017.pdf (page 18).
@@ -73,7 +62,7 @@
 #'
 #'   * `pm`: A list of 5 singletons (victims).
 #'
-#'   * `am`: A reference pedigree with 6 genotyped members and 12 missing persons
+#'   * `am`: A reference pedigree with 6 genotyped members and 12 missing persons.
 #'
 #'   * `missing`: A vector containing the names of the missing persons.
 #'   
@@ -84,7 +73,7 @@
 #' }   
 "icmp"
 
-#' Data. DVI, large pedigree
+#' Data used in the book  Kling et al. (2021)
 #'
 #' Family grave data in Kling et al. (2021) 
 #' "Mass Identifications: Statistical Methods in Forensic Genetics".
@@ -97,22 +86,24 @@
 #'
 #'   * `pm`: A list of 8 singletons (victims).
 #'
-#'   * `am`: A pedigree with 8 missing persons
+#'   * `am`: A pedigree with 8 missing persons.
 #'
 #'   * `missing`: A vector containing the names of the missing persons.
 #'   
-#' @examples   
+#' @examples
+#' \donttest{  
 #' pm = grave$pm # The list of missing persons
 #' am = grave$am # The reference family pedigree
 #' missing = grave$missing # The names of the missing persons
 #' plot(am, marker = 1)
-#' plotPedList(pm, frames = FALSE, marker = 1)
+#' pedtools::plotPedList(pm, frames = FALSE, marker = 1)
+#' }
 "grave"
 
-#' Data. Used in the book Kling et al. (2021)
+#' Data used in the book Kling et al. (2021)
 #'
-#' Data used in last example of Chapter 4  in Kling et al. (2021) 
-#' "Mass Identifications: Statistical Methods in Forensic Genetics"
+#' Data used in last example of Chapter 4 in Kling et al. (2021) 
+#' "Mass Identifications: Statistical Methods in Forensic Genetics".
 #' There are 2 female victims, 2 male victims. There are four
 #' reference families with 2 missing females and 2 missing males. 
 #' There are 21 markers. An `equal mutation mode with rate 0.005 is specified.
@@ -122,24 +113,24 @@
 #'
 #'   * `pm`: A list of 4 singletons (victims).
 #'
-#'   * `am`: A list of 3 pedigrees 
+#'   * `am`: A list of 3 pedigrees. 
 #'
 #'   * `missing`: A vector containing the names of the missing persons.
 #'   
-#'  @examples 
-#'  \donttest{
+#' @examples 
+#' \donttest{
 #'   pm = dataCh4$pm
 #'   am = dataCh4$am
 #'   missing = dataCh4$missing
 #'   res = jointDVI(pm, am, missing, disableMutations = FALSE)
 #'   head(res[c(1, 2, 30, 49),])
-#'   }
+#' }
 "dataCh4"
 
-#' Data. Exercise, Kling et al. (2021)
+#' Data used in the book Kling et al. (2021)
 #'
 #' Data used in Exercise 4.9.7 in Kling et al. (2021) 
-#' "Mass Identifications: Statistical Methods in Forensic Genetics"
+#' "Mass Identifications: Statistical Methods in Forensic Genetics".
 #' There are 3 female victims and 3 
 #' reference families with 3 missing females. 
 #' There are 23 markers, equal mutation model, rate 0.001.
@@ -149,13 +140,13 @@
 #'
 #'   * `pm`: A list of 3 singletons (victims).
 #'
-#'   * `am`: A list of 3 pedigrees 
+#'   * `am`: A list of 3 pedigrees. 
 #'
 #'   * `missing`: A vector containing the names of the missing persons.
 #'   
 "dataExercise497"
 
-#' Data. Exercise, Kling et al. (2021)
+#' Data used in the book Kling et al. (2021)
 #'
 #' Data used in Exercise 4.9.8 in Kling et al. (2021) 
 #' "Mass Identifications: Statistical Methods in Forensic Genetics".
@@ -168,13 +159,13 @@
 #'
 #'   * `pm`: A list of 3 singletons (victims).
 #'
-#'   * `am`: A list of 1 pedigree 
+#'   * `am`: A list of 1 pedigree. 
 #'
 #'   * `missing`: A vector containing the names of the missing persons.
 #'   
 "dataExercise498"
 
-#' Data. Exercise, Kling et al. (2021)
+#' Data used in the book Kling et al. (2021)
 #'
 #' Data used in Example 4.8.1 in Kling et al. (2021) 
 #' "Mass Identifications: Statistical Methods in Forensic Genetics".
@@ -193,19 +184,22 @@
 #'   * `missing`: A vector containing the names of the missing persons.
 #'   
 #' @examples 
-#' #' \donttest{
+#' \donttest{
 #' pm = dataExample481$pm
 #' am = dataExample481$am
 #' missing = dataExample481$missing
+#' 
+#' # Find number of assignments
 #' ncomb(2, 2, 0, 0)
-#' windows()
-#' plotPedList(list(pm, am), marker = 1:2, hatched = typedMembers, 
+#' 
+#' # Plot and find joint solution
+#' pedtools::plotPedList(list(pm, am), marker = 1:2, hatched = pedtools::typedMembers, 
 #'             col = list(red = missing))
 #' jointDVI(pm, am, missing, verbose = FALSE)
 #' }
 "dataExample481"
 
-#' Data. Exercise, Kling et al. (2021)
+#' Data used in the book Kling et al. (2021)
 #'
 #' Data used in Example 4.8.3 in Kling et al. (2021) 
 #' "Mass Identifications: Statistical Methods in Forensic Genetics".
@@ -227,15 +221,18 @@
 #' pm = dataExample483$pm
 #' am = dataExample483$am
 #' missing = dataExample483$missing
+#' 
+#' # Plot and find joint solution
 #' ncomb(3,2,6,6)
-#' # Make large window prior to plotting, e.g. by windows()
-#' plotPedList(list(pm, am), marker = 1:2, hatched = typedMembers, 
+#' 
+#' pedtools::plotPedList(list(pm, am), marker = 1:2, hatched = pedtools::typedMembers, 
 #'             col = list(red = missing))
 #' res = jointDVI(pm, am, missing)
+#' 
 #' # Transfer victims and check solution
-#' amId = transferMarkers(pm, am, idsFrom = paste0("V",1:5), 
+#' amId = pedtools::transferMarkers(pm, am, idsFrom = paste0("V",1:5), 
 #'                        idsTo = missing[1:5], erase = FALSE)
-#' plot(amId, marker = 1:2, hatched = typedMembers, 
+#' plot(amId, marker = 1:2, hatched = pedtools::typedMembers, 
 #'             col = list(red = missing))
 #' }
 "dataExample483"
@@ -244,7 +241,7 @@
 #'
 #' The purpose of this data is to challenge brute force methods. We use the
 #' the database NorwegianFrequencies.
-#' There are 10 male (V1, V3, ..., V19) and 10 female victims (V2, V4, ..., V20).
+#' There are 10 males (V1, V3, ..., V19) and 10 female victims (V2, V4, ..., V20).
 #' There are 10 reference families. In each family there is a genotyped grandmother
 #' and a missing grandson and a missing granddaughter. The data is simulated according to
 #' Vi = Mi, i = 1, ..., 20. 
@@ -253,24 +250,25 @@
 #'
 #'   * `pm`: A list of 20 singletons (victims).
 #'
-#'   * `am`: A list of 10 pedigrees 
+#'   * `am`: A list of 10 pedigrees. 
 #'
 #'   * `missing`: A vector containing the names of the 20 missing persons.
 #'   
-#' @examples 
+#' @examples
+#' # Remove comments to run example
 #' \donttest{
 #' pm = sibPairs$pm
 #' am = sibPairs$am
 #' missing = sibPairs$missing
 #' sequentialDVI(pm, am, missing, updateLR = TRUE)
-#' jointDVI(pm, am, missing, threshold = 100)
+#' # jointDVI(pm, am, missing, threshold = 100)
 #' 
 #' # Reduce to 15 markers. `sequentialDVI` still gives correct solutions,
 #' # but `jointDVI` struggles. Recommend sequential approach or possible to modify the joint?
 #' set1 = c("CSF1PO", "D2S1338", "D3S1358", "D5S818", "D7S820", "D8S1179", "D13S317", "D16S539",
 #'        "D18S51", "D19S433", "D21S11", "FGA", "TH01", "TPOX", "VWA")
 #' 
-#' jointDVI(pm, am, missing, markers = set1, threshold = 10)
+#' # jointDVI(pm, am, missing, markers = set1, threshold = 10)
 #' }
 "sibPairs"
 
