858c529560f8fe58fa95dc0aed09bce39023e1d2
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-11-19 16:51:12 +0100

    New internal function `estimateGridSize()`

--- R/expand.grid.nodup.R
+++ R/expand.grid.nodup.R
@@ -55,9 +55,44 @@ expand.grid.nodup = function(lst) {
   reslist = recurse(lst)
   
   # Convert to data frame and add names
-  res = as.data.frame.matrix(do.call(rbind, reslist))
+  resmat = matrix(unlist(reslist, recursive = FALSE, use.names = FALSE), 
+                  byrow = TRUE, ncol = len)
+  res = as.data.frame.matrix(resmat)
   names(res) = nms
   
   res
 }
 
+
+# Compute a lower bound on the number of assignments for a given list of pairings
+# Exact if entries of lst are all contained in each other
+estimateGridSize = function(lst) {
+  
+  n = length(lst)
+  lens = sort.int(lengths(lst))
+  
+  if(n == 0 || any(lens == 0))
+    return(0)
+  
+  allstars = all(vapply(lst, function(v) length(v) > 0 && "*" %in% v, TRUE))
+  
+  if(!allstars)
+    # Assume no stars. Trivial formula (Theorem 2 from paper 2022)
+    return(prod(lens - 0:(n-1)))
+  
+  # From here: All sets contain "*"
+  C = lens - 1
+  a = matrix(0, ncol = n + 1, nrow = n)
+  
+  # first column: a_n,0 (no stars)
+  a[, 1] = cumprod(C - 0:(n-1))
+  
+  # Diagonal: k stars
+  for(k in 1:n) 
+    a[k, k+1] = 1
+  
+  # Remaining entries: Recurse!
+  for(k in seq_len(n)) for(s in seq_len(k-1))
+    a[k,s+1] = a[k-1, s] + a[k-1, s+1] * (C[k] - (k-1-s))
+  
+  sum(a[n, ])
