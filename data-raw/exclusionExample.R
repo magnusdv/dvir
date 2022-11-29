f75dd150561a25ddd118ee35dc2b2f02a962210d
Thore <thore.egeland@gmail.com>
2022-11-29 08:17:15 +0100

    Added exclusionExample.fam to data-raw

--- data-raw/exclusionExample.R
+++ data-raw/exclusionExample.R
@@ -1,17 +1,11 @@
 # exclusionExample provided by Lourdes Prieto
 
-rm(list = ls()) # removes all R objects
-con <- url("https://familias.name/dviapp/exclusionExample.RData") 
-load(con) 
-close(con) # Finished loading data: from, to, ids.to and moves
-rm(con)
+exclusionExample = dvir::familias2dvir("exclusionExample.fam",
+                                  victimPrefix ="V",
+                                  missingPrefix = "M",
+                                  refPrefix = "R")
 
 library(pedtools)
-
-# Collect and save
-exclusionExample = dviData(pm = exclusionExample$pm, 
-                           am = exclusionExample$am, 
-                           missing = exclusionExample$missing)
 usethis::use_data(exclusionExample, overwrite = TRUE)
 
 
