626a5ebc6c16ddea88c001dfc9f366af07f0ac94
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-09-28 15:19:29 +0200

    Minor tweaks of `plotUndisputed()`

--- R/plotUndisputed.R
+++ R/plotUndisputed.R
@@ -1,36 +1,34 @@
 #' Plot undisputed identifications
-#'
-#' @param dvi A `dviData` object, typically created with [dviData()].
+#' 
+#' @inheritParams plotSolution
 #' @param undisputed A data frame containing the undisputed matches, typically
-#'   the entry `undisputed` in output from [findUndisputed()] 
-#'   (only three first columns used).
-#' @param format See [plotSolution()]. 
-#' @param ... Further parameters to be passed on to [pedtools::plot.ped()],
-#'   e.g., `marker`, `cex`, `cex.main`, `symbolsize`.
+#'   the entry `undisputed` in output from [findUndisputed()] (only three first
+#'   columns used).
+#' @param ... Further arguments passed on to [plotSolution()].
 #'
 #' @return NULL
-#'
+#' 
+#' @seealso [plotSolution()]
 #' @examples
 #'
-#' # Example 
+#' # Example
 #' res = findUndisputed(example2, threshold = 2, relax = TRUE, verbose = FALSE)
 #' u = res$undisputed
 #' plotUndisputed(example2, u, marker = 1)
 #'
-#' @importFrom grDevices dev.new
-#' @importFrom graphics grconvertX grconvertY layout mtext par rect
 #' @export
-plotUndisputed = function(dvi, undisputed,  format = "[S]=[M]", ...){
+plotUndisputed = function(dvi, undisputed, ...){
   
   d = dim(undisputed)[1]
   if(d == 0)
-    stop("No undisputed to plot.")
+    stop2("No undisputed to plot.")
   
-  #Include only families with identification(s)
-  dvi$am = dvi$am[unique(undisputed[,3])]
+  # Include only families with identification(s)
+  dvi = subsetDVI(dvi, am = unique(undisputed$Family))
   
-  x = as.data.frame(matrix(nrow = 1, ncol = d))
-  x[1,] = undisputed[,2]
-  colnames(x) = undisputed[,1]
-  plotSolution(dvi, x, format = format, ...)
+  # Assignment as named vector (vic = miss)
+  a = undisputed$Missing
+  names(a) = undisputed$Sample
+
+  plotSolution(dvi, assignment = a, ...)
 }
