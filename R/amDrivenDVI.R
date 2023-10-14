a50fc5da1120fdea91d77a1c3b819f44295c2b93
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-10-14 18:01:40 +0200

    Extend `amDrivenDVI()` to non-simple families

--- R/amDrivenDVI.R
+++ R/amDrivenDVI.R
@@ -5,10 +5,8 @@
 #' have been removed.
 #' 
 #' @param dvi A `dviData` object.
-#' @param simple A logical (only TRUE for now)
-#' @param thresholdProbable LR treshold
-#' @param LRmatrix NULL
-#' @param ignoreSex FALSE
+#' @param threshold LR threshold for 'certain' match 
+#' @param threshold2 LR threshold for 'probable' match
 #' @param verbose TRUE
 #'
 #' @return A list of `dviReduced` and `summary`.
@@ -19,69 +17,81 @@
 #' # Bigger example: Undisputed first
 #' u = findUndisputed(planecrash)
 #' u$summary
-#' amDrivenDVI(u$dviReduced)
+#' 
+#' # AM-driven analysis of the remaining
+#' amDrivenDVI(u$dviReduced, threshold2 = 500)
 #' 
 #' @export
-amDrivenDVI = function(dvi, simple = TRUE, thresholdProbable = 10^3, 
-                       LRmatrix = NULL, ignoreSex = FALSE, verbose = TRUE) {
+amDrivenDVI = function(dvi, threshold = 1e4, threshold2 = 10^3, verbose = TRUE) {
   dvi = consolidateDVI(dvi)
   
-  # Enforce family names
+  if(verbose)
+    cat("AM-driven analysis\n")
+  
+  # Enforce family names   # TODO: Move to consolidateDVI()?
   if(is.null(names(dvi$am)))
     dvi = relabelDVI(dvi, familyPrefix = "")
   
-  if(!simple)
-    stop2("Only simple families are supperted for now")
+  famnames = names(dvi$am)
   
-  simple = getSimpleFams(dvi)
-  if(!length(simple))
-    stop2("No simple reference families in the dataset\n")
-  if(verbose)
-    cat("Identified simple families:", toString(simple), "\n")
+  # Number of missing in each fam
+  comp = getFamily(dvi, ids = dvi$missing)
+  nMiss = sapply(famnames, function(fam) sum(comp == fam))
+  names(nMiss) = famnames
   
-  if(is.null(LRmatrix)) {
+  # If any simple families, compute LR matrix once
+  
+  if(any(nMiss == 1)) {
+    simple = names(which(nMiss == 1))
     .dviRed = subsetDVI(dvi, am = simple, verbose = FALSE)
     LRmatrix = pairwiseLR(.dviRed, verbose = FALSE)$LRmatrix
   }
   
-  resList = lapply(simple, function(s) {
-    dvi1 = subsetDVI(dvi, am = s, verbose = FALSE)
-    #if(verbose)
-    #  cat(sprintf("Analysing family '%s' (missing person '%s')", s, dvi1$missing), "\n")
-    r = .simpleFamDVI(dvi1, threshold = thresholdProbable, LRmatrix = LRmatrix)
+  resList = lapply(famnames, function(fam) {
+    dvi1 = subsetDVI(dvi, am = fam, verbose = FALSE)
+    if(verbose)
+      cat(sprintf(" Family %s; %d missing (%s)", fam, length(dvi1$missing), toString(dvi1$missing)))
+    if(nMiss[fam] == 1)
+      r = .simpleFamDVI(dvi1, threshold = threshold2, LRmatrix = LRmatrix)
+    else
+      r = .jointFamDVI(dvi1, threshold = threshold)
+    if(verbose)
+      cat(" -->", if(is.null(r)) "Inconclusive" else r$Conclusion[1], "\n")
+
     r
   })
   
   summary = do.call(rbind, resList)
   
-  nonsimple = setdiff(names(dvi$am), simple)
-  nam = length(nonsimple)
-  if(nam > 0)
-    dviRed = subsetDVI(dvi, am = nonsimple, verbose = FALSE)
+  remainMissing = setdiff(dvi$missing, summary$Missing)
+  remainVics = setdiff(names(dvi$pm), summary$Sample)
+  if(length(remainMissing) || length(remainVics))
+    dviRed = subsetDVI(dvi, pm = remainVics, missing = remainMissing, verbose = FALSE)
   else 
     dviRed = NULL
   
-  if(verbose) 
-    cat("Reduced dataset:", if(!nam) "Empty!" else paste(nam, if(nam==1) "family" else "families"), "\n")
-
+  if(verbose)  {
+    nam = length(dviRed$am)
+    cat("Reduced dataset:", if(nam==0) "Empty!\n" else paste(nam, if(nam==1) "family" else "families", "remaining\n"))
+  }
+  
   list(dviReduced = dviRed, summary = summary)
 }
 
-.simpleFamDVI = function(dviSimple, threshold, LRmatrix = NULL, ignoreSex = FALSE) {
+.simpleFamDVI = function(dvi1, threshold, LRmatrix = NULL) {
   
-  miss = dviSimple$missing
+  miss = dvi1$missing
   if(length(miss) != 1)
     stop2("`.simpleFamDVI()` expects a dataset with a single missing person: ", miss)
   
-  am = dviSimple$am
+  am = dvi1$am
   if(length(am) != 1 || is.null(names(am))) {
-    print(dviSimple)
+    print(dvi1)
     stop2("`.simpleFamDVI()` expects a `dviData` object with a single, named, family")
   }
   
   if(is.null(LRmatrix))
-    LRmatrix = pairwiseLR(dviSimple, ignoreSex = ignoreSex, 
-                          check = FALSE, verbose = FALSE)$LRmatrix
+    LRmatrix = pairwiseLR(dvi1, check = FALSE, verbose = FALSE)$LRmatrix
   
   # Row of LRs for the missing person
   lrs = LRmatrix[, miss]
@@ -119,6 +129,39 @@ amDrivenDVI = function(dvi, simple = TRUE, thresholdProbable = 10^3,
     maxLR = NA
   }
   
-  data.frame(Family = names(dviSimple$am), Missing = miss, Sample = bestMatch, 
+  data.frame(Family = names(dvi1$am), Missing = miss, Sample = bestMatch, 
              LR = maxLR, Conclusion = concl, Comment = comment)
+}
+
+
+.jointFamDVI = function(dvi1, threshold, ...) {
+  fam = names(dvi1$am)
+  
+  j = jointDVI(dvi1, undisputed = FALSE, ..., verbose = FALSE)
+  
+  # Require both LR_1 >= threshold AND LR_1:2 >= threshold
+  lrs = j$LR
+  good = lrs[1] >= threshold && lrs[1]/lrs[2] >= threshold
+  
+  if(good) {
+    # Compactify joint data frame
+    res = compactJointRes(j[1, ])
+    
+    # Number of pairs in solution
+    n = ncol(res) - 3 
+    vics = names(res)[seq_len(n)]
+    miss = as.character(res[1, seq_len(n)])
+
+    # Character with identified pairs
+    prs = sprintf("%s=%s", miss,vics)
+    
+    summary = data.frame(Family = fam, Missing = miss, Sample = vics, LR = lrs[1],
+               Conclusion = "Jointly undisputed",
+               Comment = paste("Joint with", sapply(seq_along(prs), function(i) toString(prs[-i]))))
+  }
+  else {
+    summary = NULL
+  }
+  
+  summary
 }
