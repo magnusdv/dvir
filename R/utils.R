fa57b58c84ffd83ed4c0ff16a17808ed3e599cca
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-10-15 20:31:29 +0200

    Rewrite and document `combineSummaries()`

--- R/utils.R
+++ R/utils.R
@@ -54,10 +54,31 @@ sprintfNamed = function(fmt, ...) {
   do.call(sprintf, append(arglist, fmt, 0))
 }
 
+#' Combine summary tables
+#'
+#' Combines summary tables from various functions into a final result table.
+#'
+#' @param dfs A list of data frames.
+#' @param orderBy A character with column names to sort by.
+#' @param dvi A `dviData` object used for sorting. Note that if given, this must
+#'   contain all victims and families.
+#'
+#' @return A data frame.
+#'
+#' @examples
+#' u = findUndisputed(planecrash)
+#' a = amDrivenDVI(u$dviReduced, threshold2 = 500)
+#'
+#' u$summary
+#' a$summary
+#'
+#' combineSummaries(list(u$summary, a$summary),
+#'                  orderBy = c("Family", "Missing"),
+#'                  dvi = planecrash)
 #' @export
-combineSummaries = function(..., orderBy = NULL) {
-  dfs = list(...)
-  allCols = unique(unlist(lapply(dfs, names)))
+combineSummaries = function(dfs, orderBy = NULL, dvi = NULL) {
+  allCols = c("Family", "Missing", "Sample","LR", "Conclusion", "Comment")
+  #unique(unlist(lapply(dfs, names)))
   
   # Harmonize data frames to have all columns
   dfsExt = lapply(dfs, function(df) {
@@ -73,8 +94,22 @@ combineSummaries = function(..., orderBy = NULL) {
   })
   
   final = do.call(rbind, dfsExt)
-  if("Family" %in% names(orderBy) && "Missing" %in% names(orderBy))
-    final = final[order(match(final$Family, orderBy$Family),
-                        match(final$Missing, orderBy$Missing)), , drop = FALSE]
+  if(is.null(orderBy))
+    return(final)
+  
+  if(is.null(dvi)) {
+    final = final[do.call(order, final[orderBy]), , drop = FALSE]
+    return(final)
+  }
+  
+  # Use ordering in provided DVI object
+  ordvec = lapply(orderBy, function(cc) switch(cc,
+    Family = match(final$Family, names(dvi$am)),
+    Missing = match(final$Missing, dvi$missing),
+    Sample = match(final$Sample, names(dvi$pm)),
+    stop2("Column does not exist: ", cc)))
+  
+  final = final[do.call(order, ordvec), , drop = FALSE]
+  rownames(final) = NULL
   final
 }
