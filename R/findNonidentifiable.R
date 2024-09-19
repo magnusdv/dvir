8fd0418783f35fd1aa447232c9e418492badb746
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2024-09-19 15:19:27 +0200

    Improve the algorithm finding nonidentifiable individuals

--- R/findNonidentifiable.R
+++ R/findNonidentifiable.R
@@ -1,12 +1,13 @@
 #' Nonidentifiable missing persons
 #'
-#' A missing person in a DVI case is *nonidentifiable* if unrelated to all
-#' (genotyped) reference individuals and all other missing persons in the
-#' reference family. It is often wise to ignore such individuals in [jointDVI()]
-#' and other analyses, to relieve the computational burden.
+#' A missing person in a DVI case is nonidentifiable if they are not related to
+#' any (genotyped) reference individuals, neither directly nor through other
+#' missing persons in the family. This function identifies such individuals, and
+#' reduces the DVI dataset accordingly.
 #'
-#' The implementation uses `ribd::kinship()` to identify individuals having
-#' kinship coefficient 0 with all relevant individuals.
+#' Two individuals are unrelated if their kinship coefficient is zero. The
+#' implementation uses `ribd::kinship()` to calculate all pairwise kinship
+#' coefficients.
 #'
 #' @param dvi A `dviData` object, typically created with [dviData()].
 #'
@@ -31,49 +32,62 @@
 #' plotDVI(dvi, textAbove = c(A = "nonidentif."))
 #'
 #' findNonidentifiable(dvi)
-#' 
+#'
 #' @export
 findNonidentifiable = function(dvi) {
   dvi = consolidateDVI(dvi)
+  miss = dvi$missing
+  refs = typedMembers(dvi$am)
   
   # Kinship matrix of refs and missing
   ids = c(typedMembers(dvi$am), dvi$missing)
   k = ribd::kinship(dvi$am, ids = ids)
   
-  # Hack to simplify next step: Set diagonal entries to 0
-  diag(k) = 0
-  
   # Columns with only zeroes = nonidentifiable
-  nonident = colnames(k)[colSums(k) == 0]
+  nonident = .nonidentif(k, miss, refs)
   
+  # If none, return unchanged
   if(!length(nonident))
     return(list(nonidentifiable = nonident,
                 dviReduced = dvi,
                 summary = NULL))
   
-  # Remove from `missing` slot
-  if(length(nonident))
-    dviRed = subsetDVI(dvi, missing = setdiff(dvi$missing, nonident), verbose = FALSE)
-  else
-    dviRed = dvi
+  # Reduce by removing nonidentifiable from "missing"
+  dviRed = subsetDVI(dvi, missing = setdiff(miss, nonident), verbose = FALSE)
   
   # For summary: Family of nonidentifiable indivs
   fams = getFamily(dvi, ids = nonident)
-  
-  # For summary: String of refs/missing for each
-  cmts = lapply(seq_along(nonident), function(i) {
-    idsi = intersect(ids, labels(dvi$am[[i]])) |> setdiff(nonident[i])
-    toString(idsi)
-  })
+
+  # Special comment if there are no refs in a family
+  nRefs = sapply(dvi$am, function(x) length(typedMembers(x)))
+
+  # Comment column
+  cmt = ifelse(nRefs[fams] == 0, "No references in family", "Unrelated to all references")
   
   # Build summary report
   summary = data.frame(Family = fams,
                        Missing = nonident,
                        Conclusion = "Nonidentifiable",
-                       Comment = paste("Unrelated to", unlist(cmts, use.names = FALSE)),
+                       Comment = cmt,
                        row.names = NULL)
                         
   list(nonidentifiable = nonident,
        dviReduced = dviRed,
        summary = summary)
+}
+
+.nonidentif = function(k, miss, refs) {
+  # Candidates for being nonidentifiable
+  cand = miss
+  
+  # Individuals linked to the refs
+  linked = refs
+  
+  # Iteratively remove those who are related to the previous links
+  while(length(cand) > 0 && length(linked) > 0) {
+    linked = names(which(rowSums(k[cand, linked, drop = FALSE]) > 0))
+    cand = setdiff(cand, linked)
+  }
+  
+  cand
 }
