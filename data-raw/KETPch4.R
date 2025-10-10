72ef4782b20bb6e083361945501ebb47b5f9c2ad
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2025-10-10 20:27:41 +0200

    Update dataset `KETPch4`

--- data-raw/KETPch4.R
+++ data-raw/KETPch4.R
@@ -1,28 +1,25 @@
+# Example from Ch4 of Mass Identifications (KETP)
+
 x = pedFamilias::readFam("data-raw/Ch4-example-4-8-4.fam")
-V1 = branch(x$H1[[1]],"V1")
-V2 = branch(x$H1[[2]],"V2")
-V3 = branch(x$H1[[4]],"V3")
-V4 = branch(x$H1[[3]],"V4")
-pm = list(V1, V2, V3, V4)
 
-R1 = branch(x$H1[[1]],"R1")
-R2 = branch(x$H1[[2]],"R2")
-R3 = branch(x$H1[[3]],"R3")
+# Quick overview
+plot(x, hatched = typedMembers, marker = 1)
 
-am1 = nuclearPed(father = "R1", mother  = "MO1", child = "MP1", sex = 2) |> 
-  transferMarkers(from = R1, to = _)
-
-am2 = nuclearPed(father = "R2", mother  = "GM2", child = "MP2") |> 
-  addSon(parents = c("MP2", "MO2"), id= "MP3") |> 
-  transferMarkers(from = R2, to = _)
-
-am3 = nuclearPed(father = "GF3", mother  = "GM3", 
-                 children = c("FA3", "MP4"), sex = c(1,2)) |> 
-  addSon(parents = c("FA3", "MO3"), id = "R3") |> 
-  transferMarkers(from = R3, to = _)
-
-am = list(am1, am2, am3)
+vics    = paste0("V",  1:4)
 missing = paste0("MP", 1:4)
+
+# Extract pm and am datasets
+pm = extractSingletons(x, vics)
+
+am = list(
+  F1 = x[[1]], 
+  F2 = mergePed(x[[2]], x[[4]], by = c(V2 = "added_1")), 
+  F3 = x[[3]]) 
+
+# Remove genotypes from victims and relabel them
+am = am |> 
+  removeGenotypes(ids = vics) |> 
+  relabel(old = vics, new = missing) 
 
 # Collect and save
 KETPch4 = dviData(pm = pm, am = am, missing = missing)
@@ -32,6 +29,6 @@ usethis::use_data(KETPch4, overwrite = TRUE)
 # Check
 if(FALSE){
   plotDVI(KETPch4, marker = 1:2)
-  res = jointDVI(KETPch4, disableMutations = FALSE)
-  res[c(1,2,30,49),]
+  res = dviSolve(KETPch4)
+  res
 }
