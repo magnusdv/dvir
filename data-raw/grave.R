359bbb935695871a83686104452cc10993f6c4fd
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2026-05-14 13:57:14 +0200

    Reorder pedigrees in datasets `grave` and `icmp`

--- data-raw/grave.R
+++ data-raw/grave.R
@@ -12,6 +12,13 @@ missing = ids.to
 old = labels(am)[c(1:5,7:8,16,20:21)]
 new = c("MO", "FA", "GM", "GF", "GF2", "MO3", "FA2", "UN2", "GM3", "FA4")
 am = relabel(am, new, old)
+
+# More sensible ordering
+ord = c("GF", "GM", "R1", "FA2", "MP1", "MP2", "R2", "MP3", "FA", 
+        "R3", "GF2", "MO", "MP4", "MP5", "FA4", "R4", "UN2", "MO3", 
+        "R5", "MP6", "MP7", "GM3", "MP8")
+am = reorderPed(am, ord)
+
 plot(am)
 
 # Collect and save
--- data-raw/icmp.R
+++ data-raw/icmp.R
@@ -3,20 +3,26 @@
 
 library(dvir)
 
+vics = paste0("V", 1:5)
+refs = c(paste0("R", 1:6))
+missing = paste0("M", 1:12)
+
+CODIS = readFreqDatabase("data-raw/codis.txt")
+
 # Reference pedigree
 df = read.table(header = TRUE, text = 
 "id fid mid sex
-  3   0   0   1
-  4   0   0   2
- M2   3   4   2
- R2   3   4   1
- R3   3   4   1
- R4   3   4   1
   1   0   0   1
   2   0   0   2
+  3   0   0   1
+  4   0   0   2
   5   1   2   1
  M1   0   0   2
  R1   1   2   1
+ R2   3   4   1
+ R3   3   4   1
+ R4   3   4   1
+ M2   3   4   2
  M3   5  M1   1
  M4   5  M1   1
  R5   0   0   2
@@ -28,18 +34,13 @@ df = read.table(header = TRUE, text =
  M9  M4  R5   2
 M10  M5  M6   1
 M11  M5  M6   1
-M12  M5  M6   2")
+M12  M5  M6   2") 
 
-vics = paste0("V", 1:5)
-refs = c(paste0("R", 1:6))
-missing = paste0("M", 1:12)
+am = df |> 
+  as.ped() |> 
+  setMarkers(locusAttributes = CODIS) |> 
+  forrel::profileSim(ids = c(refs, missing), seed = 42)
 
-CODIS = readFreqDatabase("data-raw/codis.txt")
-
-# Simulate PM and AM data
-refped = setMarkers(as.ped(df), locusAttributes = CODIS)
-
-am = forrel::profileSim(refped, N = 1, ids = c(refs, missing), seed = 42)
 pm = singletons(vics, sex = c(2,1,2,1,2))
 
 # Transfer from true solution
